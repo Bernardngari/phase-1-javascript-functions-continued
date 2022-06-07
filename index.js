00ac13ac1506962d2bacde2ec5a65054a8b6a557
Bernardngari <bernardngari.ke@gmail.com>
2022-06-07 19:12:47 +0300

    commit tree e863d84c09ec197108a6fe0df52df66e56123cae

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const saturdayFun = (a="roller-skate") => {
+  return `This Saturday, I want to ${ a }!`;
+}  
+
+const mondayWork = (event='go to the office') => {
+  return `This Monday, I will ${event}.`
+}
+
+const wrapAdjective = (init="*")  => {
+  return function wrapper (a="a hard worker"){
+    return `You are ${init}${a}${init}!`
+  }
+}
